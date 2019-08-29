3b17ccad2dad1b39b43e9b8c7efd0f41fbe19418
jjankovich1999 <jjankovich1999@gmail.com>
2019-08-29 11:55:20 -0500

    Update hello.c
    Changed title, added collaborator's name

--- hello.c
+++ hello.c
@@ -1,12 +1,13 @@
 /**
   Jared T. Jankovich
   29 Aug 2019
-  Hack 01: Hello World!
+  Hack 01
 6 *
 7 */
 #include <stdlib.h>
 #include <stdio.h>
 int main(){
-  printf("My name is JT, and my major is Computer Science");
+  printf("My name is JT, and my major is Computer Science\n");
+  printf("I'm working with Levi Tran, and his major is also Computer Science");
   return 0;
 }
